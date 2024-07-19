aa4b2cc4590f67bc24ee7dbb4526233facaed00a
Juan <Juan@DESKTOP-MGG7DTP>
2024-07-19 11:34:44 -0500

    cambio el productDto para que los controlladores reciban directamente el comando

--- ShopEase.Presentation/Controllers/ProductsController.cs
+++ ShopEase.Presentation/Controllers/ProductsController.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Mvc;
 using ShopEase.Application.Features.Products.Commands.CreateProduct;
 using ShopEase.Application.Features.Products.Commands.DeleteProduct;
 using ShopEase.Application.Features.Products.Commands.UpdateProduct;
-using ShopEase.Domain.DTOs;
 using ShopEase.WebApi.Controllers.Base;
 
 namespace ShopEase.Presentation.Controllers;
@@ -17,40 +16,23 @@ public class ProductsController : ApiController
     }
 
     [HttpPost]
-    public async Task<IActionResult> RegisterProduct([FromBody]CreateProduct request, CancellationToken cancellationToken)
+    public async Task<IActionResult> RegisterProduct([FromBody] CreateProductCommand command, CancellationToken cancellationToken)
     {
-        var command = new CreateProductCommand(
-            request.Name,
-            request.Description,
-            request.Quantity,
-            request.Price,
-            request.CategoryId);
-
         var result = await Sender.Send(command, cancellationToken);
-
         return result.Success? Ok() : BadRequest(result.ErrorMessage);
     }
 
     [HttpPut]
-    public async Task<IActionResult> UpdateProduct([FromBody]UpdateProduct request, CancellationToken cancellationToken)
+    public async Task<IActionResult> UpdateProduct([FromBody] UpdateProductCommand command, CancellationToken cancellationToken)
     {
-        var command = new UpdateProductCommand(
-            request.productId,
-            request.Quantity,
-            request.Price);
-
         var result = await Sender.Send(command, cancellationToken);
         return result.Success ? Ok() : BadRequest(result.ErrorMessage);
     }
 
     [HttpDelete("{id}")]
-    public async Task<IActionResult> RemoveProduct([FromBody]DeleteProduct request,CancellationToken cancellationToken)
+    public async Task<IActionResult> RemoveProduct([FromBody] DeleteProductCommand command, CancellationToken cancellationToken)
     {
-        var command = new DeleteProductCommand(
-            request.productId);
-
         var result = await Sender.Send(command, cancellationToken);
-
         return result.Success ? Ok() : BadRequest(result.ErrorMessage);
     }
 
